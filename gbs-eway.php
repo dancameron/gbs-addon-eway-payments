362cf5164d84b2de6bf071a68fbdb1b6e151dc7a
Dan Cameron <dan@sproutventure.com>
2012-09-21 11:51:51 -0700

    eWAY Payment Processor
    Beta release and not for prime-time.

---
+++ gbs-eway.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Group Buying Payment Processor - eWay Direct Payments
+Version: Beta 1
+Plugin URI: http://sproutventure.com/wordpress/group-buying
+Description: eWay Direct Payments Add-on.
+Author: Sprout Venture
+Author URI: http://sproutventure.com/wordpress
+Plugin Author: Dan Cameron
+Contributors: Dan Cameron
+Text Domain: group-buying
+Domain Path: /lang
+*/
+
+add_action('gb_register_processors', 'gb_load_eway');
+
+function gb_load_eway() {
+	require_once('groupBuyingeWay.class.php');
+}
